230d249252fa90b8dce54c2ae7fc805b14206cc3
rizalmab <rizalmahadiab@gmail.com>
2022-01-19 11:58:48 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>EPL Tracker</h1>
+      {/* Navbar 
+      EPL widget table 
+      Dropdown*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
