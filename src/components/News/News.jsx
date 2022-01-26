2aec8944aa142785ed49503e453e18d2c3901e44
rizalmab <rizalmahadiab@gmail.com>
2022-01-26 16:31:25 +0800

    Made some changes

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,16 +2,13 @@ import React, { useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
 import Container from "react-bootstrap/Container";
 import CardGroup from "react-bootstrap/CardGroup";
-// import newsData from "../../footballdata/newsdata";
-
-// const newsDataArr = newsData.articles;
 
 const News = () => {
   const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
     fetch(
-      "https://newsapi.org/v2/top-headlines?country=gb&q=football&category=sports&apiKey=8cd8af422f8b42dba4037103e89de9a5"
+      `https://newsapi.org/v2/top-headlines?country=gb&q=football&category=sports&apiKey=8cd8af422f8b42dba4037103e89de9a5`
     )
       .then((res) => res.json())
       .then((data) => {
